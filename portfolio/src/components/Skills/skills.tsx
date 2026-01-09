1a91776e4474ff28a8699caa9ecb26e52747432e
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2026-01-09 15:04:34 -0300

    tenth commit

--- portfolio/src/components/Skills/skills.tsx
+++ portfolio/src/components/Skills/skills.tsx
@@ -1,39 +1,39 @@
 import EnglishIcon from './../../assets/englishIcon.webp'
 import JavaScriptIcon from './../../assets/javascript icon.webp'
 import ReactIcon from './../../assets/reactIcon.png'
-import TypeScript from './../../assets/typescript.webp'
+import TypeScriptIcon from './../../assets/typescript.webp'
 import PortugueseIcon from './../../assets/portugueseIcon.webp'
-import { useState } from "react";
 import './skills.css'
 
 interface SectionProps { 
-    refProp: React.RefObject<HTMLDivElement | null>; 
+  refProp: React.RefObject<HTMLDivElement | null>; 
 }
 
-function Skills({refProp}: SectionProps){
-    const [language, setLanguage] = useState([
-        {skill: "English", icon: EnglishIcon}, 
-        {skill: "Portuguese", icon: PortugueseIcon},
-        {skill: "React", icon: ReactIcon},
-        {skill: "TypeScript", icon: TypeScript},
-        {skill: "JavaScript", icon: JavaScriptIcon},
-    ])
-    setLanguage
-    return(
-        <>
-        <div ref={refProp} className="skills">
-            <div className="languages">
-                <h1>Skills</h1>
-                <div className="languagesP">
-                    {language.map((language) => (<div className={'languageSS'} key={language.skill}>
-                        <img src={language.icon} alt={language.icon} />
-                        <h2>{language.skill}</h2>
-                    </div>))}
-                </div>
+function Skills({ refProp }: SectionProps) {
+
+  const language = [
+    { skill: "English", icon: EnglishIcon }, 
+    { skill: "Portuguese", icon: PortugueseIcon },
+    { skill: "React", icon: ReactIcon },
+    { skill: "TypeScript", icon: TypeScriptIcon },
+    { skill: "JavaScript", icon: JavaScriptIcon },
+  ]
+
+  return (
+    <div ref={refProp} className="skills">
+      <div className="languages">
+        <h1>Skills</h1>
+        <div className="languagesP">
+          {language.map((lang) => (
+            <div className="languageSS" key={lang.skill}>
+              <img src={lang.icon} alt={lang.skill} />
+              <h2>{lang.skill}</h2>
             </div>
+          ))}
         </div>
-        </>
-    )
+      </div>
+    </div>
+  )
 }
 
-export default Skills;
+export default Skills
--- portfolio/src/components/projects/projects.tsx
+++ portfolio/src/components/projects/projects.tsx
@@ -1,29 +1,28 @@
-import { useState } from 'react';
 import Project1 from './../../assets/anime.png'
 import Project2 from './../../assets/login screen.png'
 import './projects.css'
 
 interface SectionProps { 
-    refProp: React.RefObject<HTMLDivElement | null>; 
+  refProp: React.RefObject<HTMLDivElement | null>; 
 }
 
-function Projects({refProp}: SectionProps){
+function Projects({ refProp }: SectionProps) {
 
-    const [projects, setProjects] = useState([
-        {project: "Anime Login Screen", image: Project1},
-        {project: "Milimalistic Login Screen", image: Project2}
-    ])
-    setProjects
-    return(
-        <>
-        <div ref={refProp} className="projects-container">
-            {projects.map((project) => (<div className='project-portfolio' key={project.project}>
-                <img src={project.image} alt={project.image} />
-                <h2>{project.project}</h2>
-            </div>))}
+  const projects = [
+    { project: "Anime Login Screen", image: Project1 },
+    { project: "Minimalistic Login Screen", image: Project2 }
+  ]
+
+  return (
+    <div ref={refProp} className="projects-container">
+      {projects.map((project) => (
+        <div className="project-portfolio" key={project.project}>
+          <img src={project.image} alt={project.project} />
+          <h2>{project.project}</h2>
         </div>
-        </>
-    )
+      ))}
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
