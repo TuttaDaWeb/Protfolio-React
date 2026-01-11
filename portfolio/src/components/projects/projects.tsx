9854ef902ee26f506292e3bb92032653fbc2e2c7
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2026-01-11 10:54:45 -0300

    seventeenth commit

--- portfolio/src/components/projects/projects.tsx
+++ portfolio/src/components/projects/projects.tsx
@@ -1,5 +1,6 @@
 import Project1 from './../../assets/anime.png'
 import Project2 from './../../assets/login screen.png'
+import Project3 from './../../assets/DayTradeBookScreen.png'
 import './projects.css'
 
 interface SectionProps { 
@@ -9,16 +10,37 @@ interface SectionProps {
 function Projects({ refProp }: SectionProps) {
 
   const projects = [
-    { project: "Anime Login Screen", image: Project1, link: 'https://github.com/TuttaDaWeb/Project-Login-Screen' },
-    { project: "Minimalistic Login Screen", image: Project2, link: 'https://github.com/TuttaDaWeb/Sistema-de-Login' }
+    { 
+      project: "Anime Login Screen",
+      image: Project1,
+      description: 'The Creation of This Login Page was to solve a problem, its about impressing others and find out how to make an Anime-style login Page',
+      tools: 'React - JavaScript',
+      link: 'https://github.com/TuttaDaWeb/Project-Login-Screen'
+    },{ 
+      project: "Minimalistic Login Screen", 
+      image: Project2,
+      description: 'This is one of my oldest projects, I developed it in order to simulated a login and register page, I also used frameworks I no longer use',
+      tools: 'HTML - CSS - Python - Flask',
+      link: 'https://github.com/TuttaDaWeb/Sistema-de-Login' 
+    },{ 
+      project: "Day Trade Book Landing Page", 
+      image: Project3,
+      description: 'The Development of this website was to solve a problem of an attracted client, They wanted a Landing Page for his Day Trade e-book',
+      tools: 'HTML - CSS - React - JavaScript',
+      link: 'https://github.com/TuttaDaWeb/My-First-Job' 
+    }
   ]
 
   return (
     <div ref={refProp} className="projects-container">
       {projects.map((project) => (
-        <div className={`project-portfolio`} key={project.project} onClick={() => window.open(project.link)}>
+        <div className='project-portfolio'key={project.project} onClick={() => window.open(project.link)}>
           <img src={project.image} alt={project.project} />
           <h2>{project.project}</h2>
+          <div className="portfolio-desc">
+            <h5>{project.description}</h5>
+          </div>
+          <p>{project.tools}</p>
         </div>
       ))}
     </div>
