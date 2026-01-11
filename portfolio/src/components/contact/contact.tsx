34c969329ffdbd607751e7f0359b8700501ff914
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2026-01-11 11:07:28 -0300

    eighteenth commit

--- portfolio/src/components/contact/contact.tsx
+++ portfolio/src/components/contact/contact.tsx
@@ -11,11 +11,12 @@ function Contact({refProp}: SectionProps){
             <h1>Contact</h1>
             <div className="card">
                 <div className="card-content">
-                    <div className="card-title">Contact</div>
+                    <div className="card-title">Want a React Developer?</div>
                     <div className="card-informations">
                         <div className="phone">Whatsapp: <span>+55 11 99173-8249</span></div>
                         <div className="e-mail">E-mail: arthur.vieira2007@hotmail.com</div>
                         <div className="federal-unit">State: Sao Paulo</div>
+                        <div className="message-short">Interested in me? You're free to reach out</div>
                     </div>
                 </div>
             </div>
