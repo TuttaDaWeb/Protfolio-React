bb053f2f7ba45b9e5f42fd735fc862bb49075cd1
ARTHUR VIEIRA <arthur.vieira2007@hotmail.com>
2026-01-15 17:26:40 -0300

    twientie-fifth commit

--- portfolio/src/components/mainPart/mainPart.tsx
+++ portfolio/src/components/mainPart/mainPart.tsx
@@ -1,4 +1,5 @@
 import Picture from './../../assets/profile-picture-2.jpeg'
+import ResumeArthur from './../../assets/Arthur V. Resume.pdf'
 import './mainPart.css'
 
 interface SectionProps {  
@@ -18,6 +19,7 @@ function Main({refProp}: SectionProps){
                 <div className="desc">
                     <p>I’m a front-end developer specializing in React and TypeScript, with strong skills in JavaScript, HTML, CSS, and Git. I’m passionate about building fast, modern, and responsive websites, always driven by a spirit of leadership and continuous improvement.</p>
                 </div>
+                <a href={ResumeArthur} download={ResumeArthur} target="_blank"><button className='resume-download'>Download Résumé</button></a>
             </div>
         </main>
         <div className="grid"></div>
